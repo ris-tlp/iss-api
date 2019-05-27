81456e3c447fa09aa532961ff71d4527aaedc109
ris-tlp <omarkhantlp@gmail.com>
2019-05-27 21:12:12 +0300

    fetching TLE data set script

--- iss/index.py
+++ iss/index.py
@@ -1,19 +1,8 @@
 from flask import Flask, jsonify, request
-from pyorbital
+from .nasa_scrape import scrape
 
 app = Flask(__name__)
 
-incomes = [
-    {'description': 'salary', 'amount': 5000},
-    {'description': 'salary', 'amount': 50410},
-    {'description': 'salary', 'amount': 1351350}
-]
-
-
 @app.route("/")
-def hello_world():
-    return "Hello, world!"
-
-@app.route('/incomes')
-def get_incomes():
-    return jsonify(incomes)
+def tle_data():
+    return scrape()
--- iss/nasa_scrape.py
+++ iss/nasa_scrape.py
@@ -1,1 +1,18 @@
-#placehold
+import urllib
+from bs4 import BeautifulSoup
+
+def scrape():
+    page = urllib.request.urlopen(
+        "https://spaceflight.nasa.gov/realdata/sightings/SSapplications/Post/JavaSSOP/orbit/ISS/SVPOST.html")
+
+    soup = BeautifulSoup(page, "html.parser")
+    html_list = str(soup.pre).split()
+    tle_list = []
+
+    for i, value in enumerate(html_list):
+        if(value == "SET"):
+            tle_list = html_list[i+2:i+20]
+            break
+
+    tle_data = " ".join(tle_list)
+    return tle_data
