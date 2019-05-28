6d2e74396eb3993a153591446b8ff9425447a008
ris-tlp <omarkhantlp@gmail.com>
2019-05-28 06:41:12 +0300

    mvp

--- iss/convertdata.py
+++ iss/convertdata.py
@@ -1,16 +1,17 @@
 # from pyorbital import tlefile
 import ephem
 import datetime
+from math import degrees
 
 def parse(tle_list):
-    line1 = tle_list[0]
-    line2 = tle_list[1]
-    print(line1)
-    print(line2)
+    line1 = str(tle_list[0])
+    line2 = str(tle_list[1])
+    iss = ephem.readtle("ISS (ZARYA)", line1, line2)
+    iss.compute(datetime.datetime.utcnow())
 
-    iss = ephem.readtle("ISS", str(line1), str(line2))
-    now = datetime.datetime.utcnow()
-    iss.compute(now)
-    lon = iss.sublong()
-    return lon
-    # return line1, line2
+    data = {
+        "Latitude": str(round(degrees(iss.sublat), 4)) + " N",
+        "Longitude": str(round(degrees(iss.sublong), 4)) + " E"
+    }
+
+    return data
--- iss/index.py
+++ iss/index.py
@@ -1,15 +1,12 @@
 from flask import Flask, jsonify, request
-from .nasa_scrape import scrape
+from .tle_scrape import scrape
 from .convertdata import parse
 
 app = Flask(__name__)
 
 @app.route("/")
 def tle_data():
-    # x = parse(scrape())
     tle_list = scrape()
-    x = parse(tle_list)
-    # return f'{x} \n {y}'
-    # return scrape()
-    return x
+    data = parse(tle_list)
+    return jsonify(data)
 
