8d2985206f43d4a3052b6764806eb5957cb908ee
ris-tlp <omarkhantlp@gmail.com>
2019-05-29 11:49:45 +0300

    switched to pyorbital

--- iss/convertdata.py
+++ iss/convertdata.py
@@ -1,17 +1,14 @@
-# from pyorbital import tlefile
-import ephem
+from pyorbital.orbital import Orbital
 import datetime
-from math import degrees
 
 def parse(tle_list):
     line1 = str(tle_list[0])
     line2 = str(tle_list[1])
-    iss = ephem.readtle("ISS (ZARYA)", line1, line2)
-    iss.compute(datetime.datetime.utcnow())
-
+    orb = Orbital("ISS (ZARYA)", line1=line1, line2=line2)
+    data_set = orb.get_lonlatalt(datetime.datetime.utcnow())
     data = {
-        "Latitude": str(round(degrees(iss.sublat), 4)) + " N",
-        "Longitude": str(round(degrees(iss.sublong), 4)) + " E"
+        "Longitude": round(data_set[0], 3),
+        "Latitude": round(data_set[1], 3)
     }
-
+    
     return data
