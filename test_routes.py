76f3d866c3c564e4ada41c541e7765b833110eed
ris-tlp <omarkhantlp@gmail.com>
2019-05-30 01:49:54 +0300

    added unit tests for routes

---
+++ test_routes.py
@@ -0,0 +1,22 @@
+import unittest
+from iss.index import app
+
+class RouteTests(unittest.TestCase):
+
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_home_status_code(self):
+        result = self.app.get('/')
+        self.assertEqual(result.status_code, 200)
+    
+    def test_random_status_code(self):
+        result = self.app.get('/random')
+        self.assertEqual(result.status_code, 404)
+
+if __name__ == "__main__":
+    unittest.main()
